2cc5f2fa6234f42dee921167b4d410ff4436be52
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-05-23 17:23:36 +0500

    add message model with text and image validation

---
+++ backend/src/models/Message.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      validate: {
+        validator: function () {
+          return this.text || this.image;
+        },
+      },
+    },
+    image: {
+      type: String,
+      validate: {
+        validator: function () {
+          return this.text || this.image;
+        },
+      },
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Message = mongoose.model("Message", MessageSchema);
+
+export default Message;
