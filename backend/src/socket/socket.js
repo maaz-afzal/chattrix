f46eb3c7d9f74dd4cdc09c8dc71689c8bd083b32
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-05-30 16:17:07 +0500

    add real-time messaging with socket.io

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -1,13 +1,54 @@
 import { Server } from "socket.io";
+import Message from "../models/Message.js";
+
+const userSocketMap = new Map();
 
 const initSocket = (server) => {
-  const io = new Server(server, {});
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
   io.on("connection", (socket) => {
-    console.log("User connected");
+    console.log("User connected:", socket.id);
+
+    socket.on("setup", (userData) => {
+      socket.userId = userData.id;
+
+      userSocketMap.set(userData.id, socket.id);
+
+      console.log(`User ${userData.id} connected with socket ${socket.id}`);
+    });
+
+    socket.on("send-message", async ({ receiverId, message }) => {
+      try {
+        const newMessage = await Message.create({
+          sender: socket.userId,
+          receiver: receiverId,
+          text: message,
+        });
+
+        const receiverSocketId = userSocketMap.get(receiverId);
+
+        if (receiverSocketId) {
+          io.to(receiverSocketId).emit("receive-message", newMessage);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
     socket.on("disconnect", () => {
-      console.log("User disconnected");
+      if (socket.userId) {
+        userSocketMap.delete(socket.userId);
+
+        console.log(`user ${socket.userId} removed from map`);
+      }
     });
   });
+
+  return io;
 };
 
 export default initSocket;
