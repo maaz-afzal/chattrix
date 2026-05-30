e4434a0f186699fb11108eba59911462dd28176f
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-05-30 17:32:47 +0500

    add routing with protected route guard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProfilePage from "./pages/ProfilePage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </div>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import "./index.css";
 import App from "./App.jsx";
 import store from "./redux/store.js";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 );
