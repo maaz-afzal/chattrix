d782d2c161c791afdc6eae8dfcc0abac37c1a342
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-05-30 17:10:23 +0500

    add auth slice and configure redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "./Slices/authSlice";
+
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
 
 export default store;
