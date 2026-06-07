48dc9178a3672104b408b6e180a2c55c1711431e
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-06-07 20:30:09 +0500

    add rate limiting to auth routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,18 @@
 import { register, login } from "../controllers/authController.js";
 import express from "express";
+import { rateLimit } from "express-rate-limit";
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    msg: "Too many requests from this IP, please try again after 15 minutes",
+  },
+});
+
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", apiLimiter, register);
+router.post("/login", apiLimiter, login);
 
 export default router;
