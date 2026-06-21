86ef752a49d6df55e1f3c50207f79d4cec19219f
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-06-21 15:43:34 +0500

    exit process on MongoDB connection failure

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,8 @@ const db = async () => {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
-    console.log("Error connecting to MongoDB!", err);
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1);
   }
 };
 
