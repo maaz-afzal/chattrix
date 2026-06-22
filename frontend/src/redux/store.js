27595c3f211a24d2f473086a78d6814a7b51a067
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-06-22 21:09:54 +0500

    named export added so api.js can import store directly

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import authReducer from "./Slices/authSlice";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     auth: authReducer,
   },
