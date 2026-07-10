3f6ab8b53ff7071b7b870662bb326a624a4d78be
maaz afzal <maazafzal.khattak007@gmail.com>
2026-07-11 03:07:34 +0500

    add conversation model

---
+++ backend/src/models/Conversation.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+      default: null,
+    },
+    unreadCount: {
+      type: Map,
+      of: Number,
+      default: {},
+    },
+    isAIChat: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+conversationSchema.index({ participants: 1 });
+conversationSchema.index({ updatedAt: -1 });
+
+const Conversation = mongoose.model("Conversation", conversationSchema);
+
+export default Conversation;
